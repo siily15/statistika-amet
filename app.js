6f0818ea600d81cb8a204004d6f9b0b219c8df4d
Siily15 <arnokosk15@gmail.com>
2020-03-10 11:10:54 +0200

    js

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
 let occupations ={
-    0:"Pakkijad",
-    1:"Juhid",
-    2:"Keemikud",
-    3:"Tarkavra arendajad",
-    4:"Puusepad"
+    9321:{name:'Pakkijad', rate:0},
+    1:{name: 'Juhid', rate: 0},
+    2223:{name: 'Keemikud', rate: 0},
+    2512:{name: 'Tarkavra arendajad', rate: 0},
+    7115:{name: 'Puusepad', rate: 0}
     
 }
 fetch('http://andmebaas.stat.ee/sdmx-json/data/PA633/1+36+DBL97+DBL306+DBL425.3.1/all?startTime=2014&endTime=2014&dimensionAtObservation=allDimensions')
 .then(Response =>{
     return Response.json()
 })
-.then(data =>{
+
+.then(data=>{
     console.log(data)
+
+    data.structure.dimensions.observation[0].values.forEach(el => {
+        console.log(el.name.split(''))
+    });
 })

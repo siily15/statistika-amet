2a206ab249b5cd036364679d49c09cd7da59b4f7
Siily15 <arnokosk15@gmail.com>
2020-03-11 10:49:41 +0200

    JS

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 let occupations ={
     9321:{name:'Pakkijad', rate:0},
     1:{name: 'Juhid', rate: 0},
-    2223:{name: 'Keemikud', rate: 0},
+    2113:{name: 'Keemikud', rate: 0},
     2512:{name: 'Tarkavra arendajad', rate: 0},
     7115:{name: 'Puusepad', rate: 0}
     
@@ -10,16 +10,19 @@ fetch('http://andmebaas.stat.ee/sdmx-json/data/PA633/1+36+DBL97+DBL306+DBL425.3.
 .then(Response =>{
     return Response.json()
 })
-
 .then(data=>{
     console.log(data)
 
+    
     data.structure.dimensions.observation[0].values.forEach((el, i) => {
         const occupationkey = (el.name.split(' ')[0])
-        const dataSetkey = i + ':0:0:0:'
+        const dataSetkey = i + ':0:0:0'
+        console.log(i)
         console.log(el.name.split(' ')[0])
         console.log(dataSetkey)
-        console.log(data.dataSets[0].observations[dataSetkey][0])
+        console.log(data.dataSets[0].observations[dataSetkey])
         occupations[occupationkey].rate = data.dataSets[0].observations[dataSetkey][0]
+
     });
+    console.log(occupations)
 })
